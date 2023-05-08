d1038d9f523a468a9ef61f3512ca35422d2bd5c9
Roshan Rajapakse <rosh474@gmail.com>
2023-05-08 22:47:22 +0930

    script updated

--- deploy_pythonanywhere.sh
+++ deploy_pythonanywhere.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+#set -e
+
+#echo "Deploying to PythonAnywhere"
+#echo "Username: roshanr"
+#echo "API token: 9584b23b448ceec0c89dd8aee0b5424553fad57a"
+
+#pip install pythonanywhere
+#export PATH=$PATH:$HOME/.local/bin
+#pythonanywhere --apikey "9584b23b448ceec0c89dd8aee0b5424553fad57a" webapps
+
+
+#!/bin/bash
+
 set -e
 
-echo "Deploying to PythonAnywhere"
-echo "Username: roshanr"
-echo "API token: 9584b23b448ceec0c89dd8aee0b5424553fad57a"
-
 pip install pythonanywhere
-export PATH=$PATH:$HOME/.local/bin
-pythonanywhere --apikey "9584b23b448ceec0c89dd8aee0b5424553fad57a" webapps
+python -m pythonanywhere.deploy --user roshanr --api_token 9584b23b448ceec0c89dd8aee0b5424553fad57a --project_path $TRAVIS_BUILD_DIR --domain roshanr.pythonanywhere.com
