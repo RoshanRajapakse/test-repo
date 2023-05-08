8818173e97ad5760acaa195876e69e68bcd151a6
Roshan Rajapakse <rosh474@gmail.com>
2023-05-08 22:19:03 +0930

    update pythonanywhere script

--- deploy_pythonanywhere.sh
+++ deploy_pythonanywhere.sh
@@ -7,4 +7,5 @@ echo "Username: roshanr"
 echo "API token: 9584b23b448ceec0c89dd8aee0b5424553fad57a"
 
 pip install pythonanywhere
-python -m pythonanywhere --apikey "9584b23b448ceec0c89dd8aee0b5424553fad57a" webapps
+export PATH=$PATH:$HOME/.local/bin
+pythonanywhere --apikey "9584b23b448ceec0c89dd8aee0b5424553fad57a" webapps
