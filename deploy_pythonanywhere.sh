4f2d51dbed8ee9e65180867d24a02996a0c6cae6
Roshan Rajapakse <rosh474@gmail.com>
2023-05-08 23:06:22 +0930

    script updated again

--- deploy_pythonanywhere.sh
+++ deploy_pythonanywhere.sh
@@ -13,7 +13,23 @@
 
 #!/bin/bash
 
+#set -e
+
+#pip install pythonanywhere
+#python -m pythonanywhere.deploy --user roshanr --api_token 9584b23b448ceec0c89dd8aee0b5424553fad57a --project_path $TRAVIS_BUILD_DIR --domain roshanr.pythonanywhere.com
+
+
+#!/bin/bash
 set -e
 
-pip install pythonanywhere
-python -m pythonanywhere.deploy --user roshanr --api_token 9584b23b448ceec0c89dd8aee0b5424553fad57a --project_path $TRAVIS_BUILD_DIR --domain roshanr.pythonanywhere.com
+# Install the required packages
+pip install flask
+
+# Download the PythonAnywhere helper script for Flask
+curl -O https://raw.githubusercontent.com/pythonanywhere/helper_scripts/master/bash/pa_install_webapp.py
+
+# Provide execution permissions
+chmod +x pa_install_webapp.py
+
+# Execute the helper script
+./pa_install_webapp.py --username roshanr --api_token 9584b23b448ceec0c89dd8aee0b5424553fad57a --app_name roshanr.pythonanywhere.com --source_directory $TRAVIS_BUILD_DIR
